2eec3d56c68376d2aeaf5b81f6d083bf9cd07ddf
cam <cameronayres123@gmail.com>
2025-03-16 13:42:13 +0000

    toHtml now take a single element, instead of a list.

--- app/HtmlParser.hs
+++ app/HtmlParser.hs
@@ -1,13 +1,16 @@
 module HtmlParser where
 
 
+-- Notes is there for incase I want inline stuff in future.
 data HTML = Pair Tag Notes Content
+-- A line should be in this format.
+data Line = Tag Content
 
 type Tag = String
 type Notes = String
 type Content = String
 
 
-toHtml :: [HTML] -> String
-toHtml []     = ""
-toHtml (Pair t n c:hs) = (foldr (++) [] ["<", t, " ", n, ">", c, "</", t, ">"]) ++ toHtml hs
+toHtml :: HTML -> String
+toHtml (Pair t n c) = foldr (++) [] ["<", t, " ", n, ">", c, "</", t, ">"]
+
