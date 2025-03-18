23249b599a43c470b7354f8f741071177d1baf45
cam <cameronayres123@gmail.com>
2025-03-18 16:58:46 +0000

    fromLine can now handle links.

--- app/HtmlParser.hs
+++ app/HtmlParser.hs
@@ -2,11 +2,11 @@ module HtmlParser where
 
 
 -- Notes is there for incase I want inline stuff in future.
-data HTML = Pair Tag Content
+data HTML = Pair Tag Content Notes
           | Single Tag Content
 
 instance Show HTML where
-  show (Pair t c)   = foldr (++) [] ["<", t, ">", c, "</", t, ">"]
+  show (Pair t c n) = foldr (++) [] ["<", t, " ", n, ">", c, "</", t, ">"]
   show (Single t c) = foldr (++) [] ["<", t, " ", c, ">"]
 
 type Tag = String
@@ -16,5 +16,10 @@ type Content = String
 
 fromLine :: String -> HTML
 fromLine []     = Single "br" ""
-fromLine (x:xs) | x == ';'  = Pair "h1" xs
-                | otherwise = Pair "p" xs
+fromLine (x:xs) | x == ';'  = Pair "h1" xs ""
+                | x == '/'  = buildLink xs
+                | otherwise = Pair "p" (x:xs) ""
+
+buildLink :: String -> HTML
+buildLink [] = Pair "p" "<b>EMPTY LINK!</b>" ""
+buildLink xs = Pair "a" (takeWhile (/='!') xs) ("href = " ++ ((tail . dropWhile (/='!')) xs))
