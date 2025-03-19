08917758f5e35238180f52132b673e32f6da9d6c
cam <cameronayres123@gmail.com>
2025-03-19 02:13:04 +0000

    Removed redundant brackets.

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import HtmlParser (genHtml)
 main :: IO ()
 main = do
   args <- getArgs
-  if (length args) >= 3
+  if length args >= 3
     then do input <- readFile (args !! 0)
             header <- readFile (args !! 2)
             writeFile ((args !! 1) ++ ".html") (genHtml header input)
