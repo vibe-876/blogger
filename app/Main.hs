c8f8211d61f73af123f1cdb11af729414aa63270
cam <cameronayres123@gmail.com>
2025-03-19 02:10:33 +0000

    Can now handle header files.

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
 import System.Environment
-import HtmlParser (renderHtml)
+import HtmlParser (genHtml)
 
 
 main :: IO ()
 main = do
   args <- getArgs
-  if (length args) >= 2
-    then readFile (args !! 0) >>= writeFile ((args !! 1) ++  ".html") . renderHtml
+  if (length args) >= 3
+    then do input <- readFile (args !! 0)
+            header <- readFile (args !! 2)
+            writeFile ((args !! 1) ++ ".html") (genHtml header input)
+                                                   
     else print "Noooooo, too few arguments!"
